0da3bd4c4deedbc29ef8805dd17642eab728bf52
Alex Landherr <alex.landherr@gmail.com>
2023-09-15 17:33:06 +0000

    Finalized printout of logo to terminal.

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include "include/constants.h"
 #include <iostream>
+#include <string_view>
+#include <thread>
+#include <chrono>
 
 int main() {
+    using namespace std::chrono_literals;
+    for (std::string_view sw : constants::logo_007) {
+        std::cout << sw << '\n';
+        std::this_thread::sleep_for(250ms);
+    }
 
     return 0;
 }
